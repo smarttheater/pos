20c5dfd598438428db24732f3a7be988dbcf2aea
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-04-25 11:09:38 +0900

    ### Addeed - 予約検索詳細追加

--- src/client/app/components/parts/reservation-detail-modal/reservation-detail-modal.component.ts
+++ src/client/app/components/parts/reservation-detail-modal/reservation-detail-modal.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { factory } from '@cinerino/api-javascript-client';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
+import { getTicketPrice } from '../../../functions';
 
 @Component({
-  selector: 'app-reservation-detail-modal',
-  templateUrl: './reservation-detail-modal.component.html',
-  styleUrls: ['./reservation-detail-modal.component.scss']
+    selector: 'app-reservation-detail-modal',
+    templateUrl: './reservation-detail-modal.component.html',
+    styleUrls: ['./reservation-detail-modal.component.scss']
 })
 export class ReservationDetailModalComponent implements OnInit {
+    @Input() public reservation: factory.chevre.reservation.event.IReservation<factory.chevre.event.screeningEvent.IEvent>;
+    public moment: typeof moment = moment;
+    public getTicketPrice = getTicketPrice;
+    constructor(
+        public activeModal: NgbActiveModal,
+        private elementRef: ElementRef
+    ) { }
 
-  constructor() { }
-
-  public ngOnInit() {
-  }
+    public ngOnInit() {
+        const element: HTMLElement = this.elementRef.nativeElement.querySelector('.scroll-vertical');
+        setTimeout(() => {
+            element.scrollTop = 0;
+        }, 0);
+    }
 
 }
