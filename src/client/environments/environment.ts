6608f8059ce9b6fc85f38586f94e3bffb70ed76c
Akito Hataguchi <hataguchi@motionpicture.jp>
2020-05-13 16:23:51 +0900

    ### Changed  - デフォルトで入場ありへ変更

--- src/client/environments/environment.ts
+++ src/client/environments/environment.ts
@@ -229,7 +229,7 @@ const defaultEnvironment: IEnvironment = {
     REGIGROW_QRCODE: '',
     DISPLAY_TICKETED_SEAT: true,
     HEADER_MENU: true,
-    HEADER_MENU_SCOPE: ['purchase', 'order', 'reservation', 'setting', 'auth'],
+    HEADER_MENU_SCOPE: ['purchase', 'admission', 'order', 'reservation', 'setting', 'auth'],
     PURCHASE_CART: false,
     PURCHASE_ITEM_MAX_LENGTH: '50',
     PURCHASE_TRANSACTION_TIME: '15',
