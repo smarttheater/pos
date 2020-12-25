639af2e934827eb6f7d3095428c69beaeae628d2
Akito Hataguchi <hataguchi@motionpicture.jp>
2020-12-25 18:15:45 +0900

    ### Changed  - QR期限を21日へ変更 ### Fixed  - ウエイターエラーハンドリング修正

--- src/client/environments/environment.ts
+++ src/client/environments/environment.ts
@@ -190,7 +190,7 @@ interface IEnvironment {
     ORDER_LINK: { name: { ja: string; en: string; }; url: string; params: { key: string; value?: string; }[] }[];
     /**
      * 注文承認コード期限(s)
-     * デフォルト943887600s 21days
+     * デフォルト1814400s 21days
      */
     ORDER_AUTHORIZE_CODE_EXPIRES: string;
     /**
@@ -266,7 +266,7 @@ const defaultEnvironment: IEnvironment = {
     ORDER_CANCEL_MAIL_CUSTOM: true,
     ORDER_PRINT: true,
     ORDER_LINK: [],
-    ORDER_AUTHORIZE_CODE_EXPIRES: '943887600',
+    ORDER_AUTHORIZE_CODE_EXPIRES: '1814400',
     PRINT_QRCODE_TYPE: 'token',
     PRINT_QRCODE_CUSTOM: '',
     PRINT_LOADING: true,
